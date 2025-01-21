ab02173ba6cabdfd1127ee5661ec4aab43b97f68
Yassine <yassinejarir7@gmail.com>
2025-01-21 08:42:09 +0100

    update the structure

--- backend/src/routes/StudentRoutes.php
+++ backend/src/routes/StudentRoutes.php
@@ -1,33 +1,33 @@
 <?php
-function handleStudentRoutes($route, $studentController)
-{
-    if (preg_match('/^student\/course\/getAndPost\/(\d+)$/', $route, $matches)) {
-        $id = (int) $matches[1];
+// function handleStudentRoutes($route, $studentController)
+// {
+//     if (preg_match('/^student\/course\/getAndPost\/(\d+)$/', $route, $matches)) {
+//         $id = (int) $matches[1];
 
-        switch ($_SERVER['REQUEST_METHOD']) {
-            case 'GET':
-                $studentController->viewCourseDetails($id);
-                break;
-            case 'POST':
-                $studentController->modifierCourse($id);
-                break;
-            case 'DELETE':
-                $studentController->supprimerCourse($id);
-                break;
-            default:
-                return false;
-        }
-        return true;
-    }
+//         switch ($_SERVER['REQUEST_METHOD']) {
+//             case 'GET':
+//                 $studentController->viewCourseDetails($id);
+//                 break;
+//             case 'POST':
+//                 $studentController->modifierCourse($id);
+//                 break;
+//             case 'DELETE':
+//                 $studentController->supprimerCourse($id);
+//                 break;
+//             default:
+//                 return false;
+//         }
+//         return true;
+//     }
 
-    switch ($route) {
-        case 'student/courses/my':
-            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-                $studentController->viewMyCourses();
-                return true;
-            }
-            break;
-    }
+//     switch ($route) {
+//         case 'student/courses/my':
+//             if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+//                 $studentController->viewMyCourses();
+//                 return true;
+//             }
+//             break;
+//     }
 
-    return false;
-}
+//     return false;
+// }
